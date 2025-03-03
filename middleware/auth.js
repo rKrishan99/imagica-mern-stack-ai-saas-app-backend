c0f3761b99b17991442dee1a1f3cab20369b24ba
RajithaKWG <rkrishan894@gmail.com>
2025-03-03 16:46:15 +0530

    done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,5 @@
 import jwt from "jsonwebtoken";
 
-// const userAuth = async (req, res, next) => {
-//   const {token} = req.headers.authorization;
-
-//   if (!token) {
-//     return res
-//       .status(401)
-//       .json({ success: false, message: "No token, authorization denied" });
-//   }
-
-//   try {
-//     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-//     if(tokenDecode.id){
-//         // req.body.userID = tokenDecode.id;
-//         req.user = { id: tokenDecode.id };
-//     }else{
-//         return res.status(401).json({success: false, message: 'Access Denied'});
-//     }
-
-//     next();
-//   } catch (error) {
-//     console.error("Auth Error:", error);
-//     res.status(401).json({ success: false, message: "Token is not valid" });
-//   }
-// };
-
 const userAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
